fbd03f8b2396532b7e8f16362de9bbd8814db07c
ytsubhadip <159522165+ytsubhadip@users.noreply.github.com>
2025-11-02 19:38:49 +0530

    update

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from routes.home_bp import home_bp
 from routes.audio_process import audio_bp
+import os
 app = Flask(__name__)
 app.secret_key = "asdniubasdb45454d15545415256v5vv5v5dsdf5@4ds5"
 
@@ -8,4 +9,5 @@ app.register_blueprint( home_bp)
 app.register_blueprint(audio_bp)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
